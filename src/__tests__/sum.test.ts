0296a811107ce8dc9fe5852af9d8168d0833c0a4
Alejandro <alejandroaperez1994g@gmail.com>
2023-11-20 13:43:30 +0100

    test: add new test

--- src/__tests__/sum.test.ts
+++ src/__tests__/sum.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import {rest, sum} from "../utils/sum"
+import {multi, rest, sum} from "../utils/sum"
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3)
@@ -7,4 +7,8 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('rest 3 - 1 to equal 2', () => {
     expect(rest(3, 1)).toBe(2)
+})
+
+test('multi 3 * 1 to equal 3', () => {
+    expect(multi(3, 1)).toBe(3)
 })
--- src/utils/sum.ts
+++ src/utils/sum.ts
@@ -5,3 +5,8 @@ export function sum(a:number, b:number):number {
 export function rest(a:number, b:number):number {
     return a - b
 }
+
+
+export function multi(a:number, b:number):number {
+    return a * b
+}
